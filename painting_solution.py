29b5072c13c3360d32e3aec0c81cd67527bb5b1a
J-S00n <jason.chen20@student.tdsb.on.ca>
2026-02-17 21:23:11 -0500

    Created the solution for the painting problem

---
+++ painting_solution.py
@@ -0,0 +1,7 @@
+
+class solution():
+    def painting_problem(A, B, X, Y):
+        return  2 * max((A + X) + max(B, Y), (B + Y) + max(A, X))
+def main():
+    print(solution.painting_problem(1, 2, 3, 1))
+main()
