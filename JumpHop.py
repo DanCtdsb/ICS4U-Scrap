3fa5037c12c1269c721910f9aca6c1be8da6dadd
D:Chen <343025052@tdsb.ca>
2026-02-18 22:15:18 -0500

    fixed jumphop function so it works for finding minimum jumps

--- JumpHop.py
+++ JumpHop.py
@@ -2,6 +2,11 @@ def JumpHop(start, end, jump):
     if start >= end:
         return 0
     
-    distance = end - start
-    return distance // jump + distance % jump
-print(JumpHop(0, 9, 4))
+    distance = abs(end - start)
+    q = distance // jump
+    r = distance % jump
+    if r == 0:
+        return q
+    else:
+        return min(distance, q + 1 + (jump - r))
+print(JumpHop(0, 16, 27))
