b32375b1f7476b87f8263c9503d24c7a25c3f785
D:Chen <343025052@tdsb.ca>
2026-02-19 21:56:50 -0500

    Added content to difference_array to continue solving a basic problem

--- difference_array.py
+++ difference_array.py
@@ -7,3 +7,9 @@ def difference_array(n, L, R):
     # Define the range using a difference array
     diff[L] += 1
     diff[R + 1] -= 1 # Stop adding AFTER R which means it INCLUDES R
+
+    arr = [0] * (n + 1) # Create the final array
+    curr = 0
+
+    return arr[1:] # Because L should be >= 1, the result is to simplfy and ensure at L = 1, diff[1] = 1, diff[0] no value 
+    
